55454772b9e7ab5f95d8542aaaf8d2d4e27bf92c
David Blass <david@arktype.io>
2025-08-20 15:27:39 -0400

    add type bench

--- src/test/types.bench.ts
+++ src/test/types.bench.ts
@@ -1,3 +1,6 @@
 import { bench } from "@ark/attest";
+import type { api } from "./fake_chef/_generated/api.js";
 
-bench("ApiFromModules", () => {}).types();
+bench("ApiFromModules", () => {
+  return {} as typeof api;
+}).types([115837, "instantiations"]);
